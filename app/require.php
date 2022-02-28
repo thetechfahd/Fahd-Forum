52bc49008557d7e2e1ee2eb8e3c8c743c35c1aa3
dev global <e.imache@ledflexgroup.com>
2022-02-28 22:29:05 +0200

    File structure update

--- app/require.php
+++ app/require.php
@@ -1,9 +1,10 @@
 <?php
+    //Require libraries from folder libraries
+    require_once 'libraries/Core.php';
+    require_once 'libraries/Controller.php';
+    require_once 'libraries/Database.php';
 
-//Require Libraries from folder Libraries
+    require_once 'config/config.php';
 
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/database.php';
-
-$core = new Core();
+    //Instantiate core class
+    $init = new Core();
